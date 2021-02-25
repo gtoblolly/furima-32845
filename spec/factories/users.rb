bec7060a411bc4184adc445d6bfa1b48da686f5e
kengo <zsensikengo@gmail.com>
2021-02-25 20:19:16 +0900

    FactoryBotの修正１

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,14 +2,14 @@ FactoryBot.define do
   factory :user do
     japanese_user = Gimei.name
 
-    nickname                {Faker::Name.last_name}
+    nickname                {"フリマくん"}
     email                   {Faker::Internet.free_email}
     password                {Faker::Internet.password(min_length: 6)+ "a1"}
     password_confirmation   {password}
-    last_name               { japanese_user.last.kanji }
-    first_name              { japanese_user.first.kanji }
-    last_name_kana          { japanese_user.last.katakana }
-    first_name_kana         { japanese_user.first.katakana }
-    birthday                {Faker::Date.birthday}
+    last_name               {"てっく"}
+    first_name              {"きゃんぷ"}
+    last_name_kana          {"テック"}
+    first_name_kana         {"キャンプ"}
+    birthday                {"2018-10-30"}
   end
 end
