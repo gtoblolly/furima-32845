41920040c62df36ba44e250499a44732e1d3d57c
kengo <zsensikengo@gmail.com>
2021-02-26 20:42:27 +0900

    不要な記述の削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :user do
-    japanese_user = Gimei.name
-
     nickname                {"フリマくん"}
     email                   {Faker::Internet.free_email}
     password                {Faker::Internet.password(min_length: 6)+ "a1"}
