8fe4e6460fa53ba75e020fd337f0d8fc19ad2a44
kengo <zsensikengo@gmail.com>
2021-02-28 22:03:24 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping
+  belongs_to :area
+  belongs_to :days
   has_one_attached :image
 end
