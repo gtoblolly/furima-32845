daaa3adbd0d4835f3635cc7406a1f7ca2fc0c41a
kengo <zsensikengo@gmail.com>
2021-03-08 21:32:55 +0900

    リダイレクト機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -19,20 +20,32 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
+    @item.update(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :text, :price, :category_id, :status_id, :shipping_id, :area_id, :days_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    if current_user.id != @item.user.id
+      redirect_to action: :index
+    end
   end
 end
