28c9e9ecbbc7fc3664973911ba57eafb07b7e587
kengo <zsensikengo@gmail.com>
2021-03-22 20:57:02 +0900

    クレジットカード決済機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,8 +5,10 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_addres = OrderAddres.new(order_params)
     if @order_addres.valid?
+      pay_item
       @order_addres.save
       redirect_to root_path
     else
@@ -17,7 +19,16 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_addres).permit(:postcode, :area_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_addres).permit(:postcode, :area_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
